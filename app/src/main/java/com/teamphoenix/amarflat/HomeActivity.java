b665a4ce97fed9616304dfbf4265a1ef451d4273
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-04-01 17:22:38 +0600

    nav view, view

--- app/src/main/java/com/teamphoenix/amarflat/HomeActivity.java
+++ app/src/main/java/com/teamphoenix/amarflat/HomeActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.teamphoenix.amarflat.databinding.ActivityHomeBinding;
+
 public class HomeActivity extends AppCompatActivity {
+    ActivityHomeBinding homeBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+
+        homeBinding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(homeBinding.getRoot());
+
     }
 }
