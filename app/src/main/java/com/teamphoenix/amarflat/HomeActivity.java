b4be95fa6b82cc7469b23e85b39b6ba6b7b067c9
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-04-01 21:06:56 +0600

    navigationitem click handling, ad post ui upgrade, custom font support

--- app/src/main/java/com/teamphoenix/amarflat/HomeActivity.java
+++ app/src/main/java/com/teamphoenix/amarflat/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.teamphoenix.amarflat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+import com.teamphoenix.amarflat.Home_Fragment.HomeFragment;
+import com.teamphoenix.amarflat.Profile_Fragment.ProfileFragment;
 import com.teamphoenix.amarflat.databinding.ActivityHomeBinding;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     ActivityHomeBinding homeBinding;
 
     @Override
@@ -24,7 +32,46 @@ public class HomeActivity extends AppCompatActivity {
                 R.string.Close_Drawer);
         homeBinding.homeDrawer.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
+        homeBinding.mainNavView.setCheckedItem(R.id.nav_home);
+        homeBinding.mainNavView.setNavigationItemSelectedListener(this);
+    }
 
+    @Override
+    public void onBackPressed() {
+        getSupportFragmentManager().popBackStackImmediate();
+        if (homeBinding.homeDrawer.isDrawerOpen(GravityCompat.START)) {
+            homeBinding.homeDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_add_property:
+                startActivity(new Intent(this, PostAd.class));
+                break;
+            case R.id.nav_contact:
+                Toast.makeText(this, "Set Contact Action Here", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_search:
+                Toast.makeText(this, "set search action Here", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_language:
+                Toast.makeText(this, "set language action here", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_home:
+                HomeFragment homeFragment = new HomeFragment();
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(homeBinding.homeFragmentContainer.getId(), homeFragment)
+                        .addToBackStack(null)
+                        .commit();
+                break;
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/teamphoenix/amarflat/PostAd.java
+++ app/src/main/java/com/teamphoenix/amarflat/PostAd.java
@@ -19,6 +19,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.teamphoenix.amarflat.Adapter.PostAdPhotoViewRecycler;
+import com.teamphoenix.amarflat.databinding.ActivityPostAdBinding;
 
 import java.util.ArrayList;
 
@@ -31,13 +32,28 @@ public class PostAd extends AppCompatActivity {
     Bitmap bitmap;
     RecyclerView imageRecyclerView;
     ArrayList<Uri> postAdImageList;
+    private ActivityPostAdBinding postAdBinding;
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post_ad);
+        postAdBinding = ActivityPostAdBinding.inflate(getLayoutInflater());
+        setContentView(postAdBinding.getRoot());
 
-        
+
+        postAdBinding.postAddToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PostAd.super.onBackPressed();
+            }
+        });
+
+
         //get size type and set adapter
         size_type = findViewById(R.id.size_type);
         sizeTypeList = new ArrayList<>();
@@ -46,7 +62,7 @@ public class PostAd extends AppCompatActivity {
         sizeTypeList.add("Sq. Yd.");
         sizeTypeList.add("Marla");
         sizeTypeList.add("Marla");
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,sizeTypeList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sizeTypeList);
         size_type.setAdapter(adapter);
 
 
@@ -55,22 +71,22 @@ public class PostAd extends AppCompatActivity {
         postAdImageList = new ArrayList<>();
         PostAdPhotoViewRecycler postAdPhotoViewAdapter = new PostAdPhotoViewRecycler(getApplicationContext(), postAdImageList);
         imageRecyclerView.setAdapter(postAdPhotoViewAdapter);
-        imageRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+        imageRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
 
         //get image
         activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
             public void onActivityResult(ActivityResult result) {
-                if (result!=null){
-                    for (int i = 0; i < result.getData().getClipData().getItemCount(); i++){
+                if (result != null) {
+                    for (int i = 0; i < result.getData().getClipData().getItemCount(); i++) {
                         postAdImageList.add(result.getData().getClipData().getItemAt(i).getUri());
                     }
                     postAdPhotoViewAdapter.notifyDataSetChanged();
                 }
             }
         });
-        
+
     }
 
     public void postAd(View view) {
@@ -78,7 +94,7 @@ public class PostAd extends AppCompatActivity {
 
     public void picAdd(View view) {
         Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
         intent.setType("image/*");
         activityResultLauncher.launch(intent);
     }
