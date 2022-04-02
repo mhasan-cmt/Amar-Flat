503e5b3a64ea41ac96f70ba5d8de3446e245b3dc
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-04-02 22:43:21 +0600

    search filter ui design complete

--- app/src/main/java/com/teamphoenix/amarflat/SearchFilterActivity.java
+++ app/src/main/java/com/teamphoenix/amarflat/SearchFilterActivity.java
@@ -4,11 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.teamphoenix.amarflat.databinding.ActivitySearchFilterBinding;
+
+import org.florescu.android.rangeseekbar.RangeSeekBar;
+
 public class SearchFilterActivity extends AppCompatActivity {
+
+    private ActivitySearchFilterBinding searchFilterBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_filter);
+        searchFilterBinding = ActivitySearchFilterBinding.inflate(getLayoutInflater());
+        setContentView(searchFilterBinding.getRoot());
+        settingUpRangeSeekbars();
+
+    }
+
+    private void settingUpRangeSeekbars() {
+        searchFilterBinding.searchPriceRangeSeekbar.setRangeValues(100, 100000000,10);
+        searchFilterBinding.searchPriceRangeSeekbar.setSelectedMaxValue(100000000);
+        searchFilterBinding.searchPriceRangeSeekbar.setSelectedMinValue(0);
+
+        searchFilterBinding.searchAreaRangeSeekbar.setRangeValues(50, 100000,10);
+        searchFilterBinding.searchAreaRangeSeekbar.setSelectedMaxValue(100000000);
+        searchFilterBinding.searchAreaRangeSeekbar.setSelectedMinValue(0);
+
+        searchFilterBinding.searchSeekbarAreaMaxValue.setText(searchFilterBinding.searchAreaRangeSeekbar.getSelectedMaxValue()+ "Sq");
+        searchFilterBinding.searchSeekbarAreaMinValue.setText(searchFilterBinding.searchAreaRangeSeekbar.getSelectedMinValue()+" Sq ");
+
+        searchFilterBinding.searchAreaRangeSeekbar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener() {
+            @Override
+            public void onRangeSeekBarValuesChanged(RangeSeekBar bar, Number minValue, Number maxValue) {
+                searchFilterBinding.searchSeekbarAreaMaxValue.setText(searchFilterBinding.searchAreaRangeSeekbar.getSelectedMaxValue()+ "Sq");
+                searchFilterBinding.searchSeekbarAreaMinValue.setText(searchFilterBinding.searchAreaRangeSeekbar.getSelectedMinValue()+" Sq ");
+            }
+        });
+
+        searchFilterBinding.searchSeekbarMaxValue.setText(searchFilterBinding.searchPriceRangeSeekbar.getSelectedMaxValue()+ "BDT");
+        searchFilterBinding.searchSeekbarMinValue.setText(searchFilterBinding.searchPriceRangeSeekbar.getSelectedMinValue()+" BDT");
+
+        searchFilterBinding.searchPriceRangeSeekbar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener() {
+            @Override
+            public void onRangeSeekBarValuesChanged(RangeSeekBar bar, Number minValue, Number maxValue) {
+                searchFilterBinding.searchSeekbarMaxValue.setText(searchFilterBinding.searchPriceRangeSeekbar.getSelectedMaxValue()+" BDT");
+                searchFilterBinding.searchSeekbarMinValue.setText(searchFilterBinding.searchPriceRangeSeekbar.getSelectedMinValue()+" BDT");
+            }
+        });
     }
 }
