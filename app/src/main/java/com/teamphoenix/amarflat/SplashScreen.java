4a5a5aca54ca14f0437427101a1f5255e9518cf8
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-04-06 22:09:29 +0600

    splash upgrade

--- app/src/main/java/com/teamphoenix/amarflat/SplashScreen.java
+++ app/src/main/java/com/teamphoenix/amarflat/SplashScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
     SharedPreferences preferences;
@@ -13,12 +14,16 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         preferences = getSharedPreferences("user_data",MODE_PRIVATE);
-        if (preferences.getString("user_name","0").equals("0")){
-            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
-        }else{
-            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
-        }
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (preferences.getString("user_name","0").equals("0")){
+                    startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                }else{
+                    startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                }
+            }
+        },2000);
 
     }
 }
